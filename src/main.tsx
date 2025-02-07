fb7f1c1cc354874b6379d00253ba5db1d952cbd2
Kim jonghun <developerkimjh@gmail.com>
2025-02-07 13:17:36 +0900

    FEAT: add vercel Analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,11 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import { RecoilRoot } from "recoil";
+import { Analytics } from "@vercel/analytics/react";
 
 createRoot(document.getElementById("root")!).render(
   <RecoilRoot>
+    <Analytics />
     <App />
   </RecoilRoot>
 );
