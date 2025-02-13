6b377a8ed418da9669b107cba605bbd142e6cc6b
Kim jonghun <developerkimjh@gmail.com>
2025-02-13 10:26:29 +0900

    FEAT: add isComposing underline

--- src/utils/draw.ts
+++ src/utils/draw.ts
@@ -8,14 +8,16 @@ interface DrawTextParams {
   shouldUpdateText: boolean;
 }
 
-export function drawText({ editorManger, canvasRefs }: DrawTextParams) {
-  const lineTexts = editorManger.getCanvasData();
 export function drawText({
   editorManger,
   canvasRefs,
   shouldUpdateText,
 }: DrawTextParams) {
+  // const start = performance.now();
   const lineTexts = editorManger.getCanvasData(shouldUpdateText);
+
+  // const end = performance.now();
+  // console.log(`get lineTexts 함수 실행 시간: ${(end - start).toFixed(2)}ms`);
 
   if (!lineTexts) return;
 
@@ -28,8 +30,12 @@ export function drawText({
     ctx.clearRect(0, 0, canvas.width, canvas.height);
   }
 
+  // TODO:drawLine 최적화 필요
+  // const start1 = performance.now();
+
   for (const [page, value] of lineTexts) {
     for (let i = 0; i < value.length; i++) {
+      const lineText = value[i];
       const canvas = canvasArr[page];
       const ctx = canvas?.getContext("2d");
 
@@ -37,40 +43,60 @@ export function drawText({
 
       drawLine({
         ctx,
-        lineTexts: value[i],
+        lineText,
         selectRange: editorManger.selectRange,
+        composingIndex: editorManger.isKoreanComposing
+          ? editorManger.cursorIndex - 1
+          : undefined,
       });
     }
   }
+
+  // const end1 = performance.now();
+  // console.log(`drawLine 함수 실행 시간: ${(end1 - start1).toFixed(2)}ms`);
 }
 
 interface DrawLineParams {
   ctx: CanvasRenderingContext2D;
+  lineText: LineText;
   selectRange: SelectRange | null;
-  lineTexts: LineText;
+  composingIndex?: number;
 }
-function drawLine({ lineTexts, ctx, selectRange }: DrawLineParams) {
-  let lineX = lineTexts.x;
+function drawLine({
+  lineText,
+  ctx,
+  selectRange,
+  composingIndex,
+}: DrawLineParams) {
+  let lineX = lineText.x;
 
-  for (let i = 0; i < lineTexts.text.length; i++) {
-    const index = lineTexts.endIndex - lineTexts.text.length + 1 + i;
-    const lineText = lineTexts.text[i];
+  for (let i = 0; i < lineText.text.length; i++) {
+    const index = lineText.endIndex - lineText.text.length + 1 + i;
+    const { fontSize, text } = lineText.text[i];
 
-    ctx.font = `400 ${lineText.fontSize}px Arial`;
+    ctx.font = `400 ${fontSize}px Arial`;
 
-    const textWidth = measureTextWidth(ctx, lineText.text);
-    const textHeight = lineTexts.maxFontSize;
+    const textWidth = measureTextWidth(ctx, text);
+    const textHeight = lineText.maxFontSize;
 
     // ctx.fillStyle = "green";
-    // ctx.fillRect(lineX, lineTexts.y, textWidth, textHeight * 1.48);
+    // ctx.fillRect(lineX, lineText.y, textWidth, textHeight * 1.48);
 
+    //draw select
     if (selectRange && index >= selectRange.start && index < selectRange.end) {
       ctx.fillStyle = "#B2CEF9";
-      ctx.fillRect(lineX, lineTexts.y, textWidth, textHeight * 1.48);
+      ctx.fillRect(lineX, lineText.y, textWidth, textHeight * 1.48);
     }
 
+    //draw text
     ctx.fillStyle = "black";
-    ctx.fillText(lineText.text, lineX, lineTexts.y + textHeight);
+    ctx.fillText(text, lineX, lineText.y + textHeight);
+
+    //draw isComposing
+    if (composingIndex === index) {
+      const underlineY = lineText.y + textHeight * 1.2;
+      drawTextUnderLine({ ctx, underlineY, lineX, textWidth });
+    }
 
     lineX += textWidth;
   }
